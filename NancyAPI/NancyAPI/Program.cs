39e7b31668a7867f84ec8cfd9217195f55df4975
fsalazarnearbpo <fsalazarnearbpo>
2018-06-04 21:00:02 -0500

    Nancy First Version

---
+++ NancyAPI/NancyAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using NancyAPI;
+using Nancy.Hosting.Self;
+using NancyAPI.Services;
+
+namespace NancyAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostConfiguration hostConfigs = new HostConfiguration();
+            hostConfigs.UrlReservations.CreateAutomatically = true;
+
+            Uri uri = new Uri("http://localhost:1234");
+            var host = new NancyHost(hostConfigs, uri);
+            host.Start();
+            Console.WriteLine("Running on http://localhost:1234");
+            
+            Console.ReadLine();
+            host.Stop();
+        }
+    }
+}
